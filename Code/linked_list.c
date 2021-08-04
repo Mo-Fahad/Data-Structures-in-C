0595c65122905c68f2f60630b9b9563768a57f1f
Mo-Fahad <mohdfahad3183@gmail.com>
2021-08-04 19:02:01 +0530

    linked list delete funcctions

--- Code/linked_list.c
+++ Code/linked_list.c
@@ -10,12 +10,20 @@ int display(struct Node*);
 
 struct Node *insert_at_head(struct Node*,int);
 struct Node *insert_at_end(struct Node*,int);
+struct Node *insert_at_pos(struct Node*,int,int);
+
+struct Node *delete_at_head(struct Node*);
+struct Node *delete_at_tail(struct Node*);
+struct Node *delete_at_pos(struct Node*,int);
 
 int main() {
 
     struct Node* head = NULL;
 
-    //head = insert_at_head(head,10);
+    head = insert_at_head(head,30);
+    head = insert_at_head(head,10);
+    head = insert_at_pos(head,20,2);
+    head = delete_at_pos(head,1);
 
     display(head);
 
@@ -61,5 +69,90 @@ struct Node *insert_at_end(struct Node* head,int data) {
         ptr = ptr->next;
     }
     ptr->next = temp;
+    return head;
+}
+
+struct Node *insert_at_pos(struct Node* head,int data,int pos) {
+    struct Node *temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data=data;
+    temp->next=NULL;
+    if(head==NULL) {
+        temp->next=head;
+        return temp;
+    }
+    struct Node *ptr =  head;
+    struct Node *prev;
+    while (pos-1 && ptr)
+    {
+        prev = ptr;
+        ptr = ptr->next;
+        pos--;
+    }
+    prev->next = temp;
+    temp->next = ptr;
+    return head;
+}
+
+struct Node *delete_at_head(struct Node* head) {
+    if (head==NULL)
+    {
+        return NULL;
+    }
+    
+    struct Node *temp;
+    temp = head;
+    head = head->next;
+    temp = NULL;
+    free(temp);
+    return head;
+}
+
+struct Node *delete_at_tail(struct Node* head) {
+    if (head==NULL || head->next==NULL)
+    {
+        return NULL;
+    }
+
+    struct Node *temp;
+    temp = head;
+    while ((temp->next)->next!=NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = NULL;
+    return head;
+}
+
+struct Node *delete_at_pos(struct Node* head, int pos) {
+    if (head==NULL)
+    {
+        return NULL;
+    }
+    
+    struct Node *temp=head,*prev;
+    if (pos==1)
+    {
+        temp = head->next;
+        head = NULL;
+        free(head);
+        return temp;
+    }
+    
+
+    while (temp!=NULL && pos-1)
+    {
+        prev = temp;
+        temp = temp->next;
+        pos--;
+    }
+    if (temp==NULL)
+    {
+        return head;
+    }
+    
+
+    prev->next = temp->next;
+    temp = NULL;
+    free(temp);
     return head;
 }
